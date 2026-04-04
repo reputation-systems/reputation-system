85148804ce5b42df8ab9c2a57d82291a6fbd3d13
0xf965 <40121100+0xf965@users.noreply.github.com>
2026-04-04 16:40:54 +0200

    update

--- dist/connect.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Connects to the Nautilus wallet using the globally available ergoConnector object.
- * Updates the application's stores with the connection status, wallet address, and network.
- */
-export declare function connectNautilus(): Promise<void>;
